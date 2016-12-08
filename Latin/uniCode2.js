422e10f20f9a50ec924858a19ce48ee5ae31dc20
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-08 01:04:05 +0100

    Properly arrnage files

---
+++ Latin/uniCode2.js
@@ -0,0 +1,138 @@
+class Punct {
+    construtor() {
+	throw Error(`${new.target.name} should not be called as a constructor`)
+    }
+
+    static InvertedExclamationMark() {
+	return '\u00a1'
+    }
+
+    static CentSign() {
+	return '\u00a2'
+    }
+
+    static PoundSign() {
+	return '\u00a3'
+    }
+
+    static CurrencySign() {
+	return '\u00a4'
+    }
+
+    static YenSign() {
+	return '\u00a5'
+    }
+
+    static BrokenBar() {
+	return '\u00a6'
+    }
+
+    static SectionSign() {
+	return '\u00a7'
+    }
+
+    static DiaeresisUmlaut() {
+	return '\u00a8'
+    }
+
+    static CopyrightSign() {
+	return '\u00a9'
+    }
+
+    static FeminineOrdinalIndicator() {
+	return '\u00aa'
+    }
+
+    static LeftPointDoubleAngleQuotationMark() {
+	return '\u00ab'
+    }
+
+    static NotSign() {
+	return '\u00ac'
+    }
+
+    static SoftHyphen() {
+	return '\u00ad'
+    }
+
+    static RegisteredSign() {
+	return '\u00ae'
+    }
+
+    static Macron() {
+	return '\u00af'
+    }
+
+    static DegreeSymbol() {
+	return '\u00b0'
+    }
+
+    static PlusMinusSign() {
+	return '\u00b1'
+    }
+
+    static SuperScriptTwo() {
+	return '\u00b2'
+    }
+
+    static SuperScriptThree() {
+	return '\u00b3'
+    }
+
+    static AcuteAccent() {
+	return '\u00b4'
+    }
+
+    static MicroSign() {
+	return '\u00b5'
+    }
+
+    static PilcrowSign() {
+	return '\u00b6'
+    }
+
+    static MiddleDot() {
+	return '\u00b7'
+    }
+
+    static Cedilla() {
+	return '\u00b8'
+    }
+
+    static SuperScriptOne() {
+	return '\u00b9'
+    }
+
+    static MasculineOrdinalIndicator() {
+	return '\u00ba'
+    }
+
+    static RightPointDoubleAngleQuotation() {
+	return '\u00bb'
+    }
+
+    static VulgarFractionOneQuarter() {
+	return '\u00bc'
+    }
+
+    static VulgarFractionOneHalf() {
+	return '\u00bd'
+    }
+
+    static VulgarFractionThreeQuarters() {
+	return '\u00be'
+    }
+
+    static InvertedQuestionMark() {
+	return '\u00bf'
+    }
+    
+}
+
+console.log(Punct.InvertedQuestionMark())
+
+console.log(Punct.VulgarFractionThreeQuarters())
+
+console.log(Punct.AcuteAccent())
+
+console.log(Punct.SoftHyphen())
---
+++ Latin/unicode.js
@@ -0,0 +1,224 @@
+
+class CapitalLatinLetters {
+    constructor() {
+	throw Error(`${new.target.name} should not be called as a constructor`);
+    }
+    static OStroke() {
+	return '\u00d8'
+    }
+    
+    static UGrave() {
+	return '\u00d9'
+    }
+
+    static UAcute() {
+	return '\u00da'
+    }
+
+    static UCircumflex() {
+	return '\u00db'
+    }
+
+    static UDiaeresis() {
+	return '\u00dc'
+    }
+
+    static YAcute() {
+	return '\u00dd'
+    }
+
+    static Thorn() {
+	return '\u00de'
+    }
+
+    static AGrave() {
+	return '\u00c0'
+    }
+    static AAcute() {
+	return '\u00c1'
+    }
+    static ACircumflex() {
+	return '\u00c2'
+    }
+    static ATilde() {
+	return '\u00c3'
+    }
+    static ADiaeresis() {
+	return '\u00c4'
+    }
+    static ARingAbove() {
+	return '\u00c5'
+    }
+    static AE() {
+	return '\u00c6'
+    }
+    static CCedilla() {
+	return '\u00c7'
+    }
+    static EGrave() {
+	return '\u00c8'
+    }
+    static EAcute() {
+	return '\u00c9'
+    }
+    static ECircumflex() {
+	return '\u00ca'
+    }
+    static EDiaeresis() {
+	return '\u00cb'
+    }
+    static IGrave() {
+	return '\u00cc'
+    }
+    static IAcute() {
+	return '\u00cd'
+    }
+    static ICircumflex() {
+	return '\u00ce'
+    }
+    static IDiaeresis() {
+	return '\u00cf'
+    }
+    static DEth() {
+	return '\u00d0'
+    }
+    static NTilde() {
+	return '\u00d1'
+    }
+    static OGrave() {
+	return '\u00d2'
+    }
+    static OGrave() {
+	return '\u00d3'
+    }
+    static OAcute() {
+	return '\u00d4'
+    }
+    static OCircumflex() {
+	return '\u00d5'
+    }
+    static ODiaeresis() {
+	return '\u00d6'
+    }
+}
+
+class SmallLatinLetters {
+    constructor() {
+	throw Error(`${new.target.name} should not be called as a constructor`);
+    }
+    static SSharp() {
+	return '\u00df'
+    }
+    static AGrave() {
+	return '\u00e0'
+    }
+    static AAcute() {
+	return '\u00e1'
+    }
+    static ACircumflex() {
+	return '\u00e2'
+    }
+    static Atilde() {
+	return '\u00e3'
+    }
+    static ADiaeresis() {
+	return '\u00e4'
+    }
+    static ARing() {
+	return '\u00e5'
+    }
+    static AE() {
+	return '\u00e6'
+    }
+    static CCedilla() {
+	return '\u00e7'
+    }
+    static EGrave() {
+	return '\u00e8'
+    }
+    static EAcute() {
+	return '\u00e9'
+    }
+    static ECircumflex() {
+	return '\u00ea'
+    }
+    static EDiaeresis() {
+	return '\u00eb'
+    }
+    static IGrave() {
+	return '\u00ec'
+    }
+    static IAcute() {
+	return '\u00ed'
+    }
+    static ICircumflex() {
+	return '\u00ee'
+    }
+    static IDiaeresis() {
+	return '\u00ef'
+    }
+    static IEth() {
+	return '\u00f0'
+    }
+    static NTilde() {
+	return '\u00f1'
+    }
+    static OGrave() {
+	return '\u00f2'
+    }
+    static OAcute() {
+	return '\u00f3'
+    }
+    static OCircumflex() {
+	return '\u00f4'
+    }
+    static OTilde() {
+	return '\u00f5'
+    }
+    static ODiaeresis() {
+	return '\u00f6'
+    }
+    static DivisionSign() {
+	return '\u00f7'
+    }
+    static OStroke() {
+	return '\u00f8'
+    }
+    static UGrave() {
+	return '\u00f9'
+    }
+    static latinUAcute() {
+	return '\u00fa'
+    }
+    static YCircumflex() {
+	return '\u00fb'
+    }
+    static UDiaeresis() {
+	return '\u00fc'
+    }
+    static YAcute() {
+	return '\u00fd'
+    }
+    static Thorn() {
+	return '\u00fe'
+    }
+    static YDiaeresis() {
+	return '\u00ff'
+    }
+    
+}
+
+class MathSym {   
+    constructor() {
+	throw Error(`${new.target.name} should not be called as a constructor`);
+    }
+    
+    static times() {
+	return '\u00d7'
+    }
+    
+    static DivisionSign() {
+	return '\u00f7'
+    }
+}
+
