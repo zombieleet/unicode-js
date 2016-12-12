e2cbe94457fd7018f3b1de9bbf234d58a5b809f3
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-13 00:44:38 +0100

    contains exported Classes from their respective js file

---
+++ unicode-js.js
@@ -0,0 +1,11 @@
+
+//Latin
+export { EuLatin, EuLatinDeprecated } from "Latin/europeanlatin.js"
+export {LatinLetters } from "Latin/latinletters.js"
+export {MathSym } from "Latin/math.js"
+export {NonEuHistLatin } from "Latin/noneuhistoriclatin.js"
+export {Punct} from "Latin/punctuation.js"
+
+
+
+// More Language support will be added here
