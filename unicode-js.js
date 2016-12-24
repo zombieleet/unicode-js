c1e8e334671ef354d9c9a8667bae7b463d10a661
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-24 19:33:05 +0100

    Require Armenia and export Armenia

--- unicode-js.js
+++ unicode-js.js
@@ -1,11 +1,40 @@
 
 //Latin
-export { EuLatin, EuLatinDeprecated } from "Latin/europeanlatin.js"
-export {LatinLetters } from "Latin/latinletters.js"
-export {MathSym } from "Latin/math.js"
-export {NonEuHistLatin } from "Latin/noneuhistoriclatin.js"
-export {Punct} from "Latin/punctuation.js"
+const { EuLatin, EuLatinDeprecated } = require("./Latin/europeanlatin.js")
+const { LatinLetters } = require("./Latin/latinletters.js")
+const { MathSym } = require("./Latin/math.js")
+const { NonEuHistLatin } = require("./Latin/noneuhistoriclatin.js")
+const { Punct } = require("./Latin/punctuation.js")
 
+
+// Armenia
+
+const { ArmeniaLetters, 
+		ModifierLetter, 
+		Punct, 
+		Symbols
+	  } = require("./Armenia/armenialetter.js");
 
 
 // More Language support will be added here
+
+
+
+
+
+
+
+// Rexporting so that the user can just require from a central file
+
+module.exports = {
+	EuLatin, 
+	EuLatinDeprecated,
+	LatinLetters,
+	MathSym,
+	NonEuHistLatin,
+	Punct,
+	ArmeniaLetters, 
+	ModifierLetter, 
+	Punct, 
+	Symbols	
+}
